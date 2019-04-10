0e2667a16456caf3450ba836db3fb01951f0cbab
Jiang <15328044115@163.com>
2019-04-10 23:51:29 +0800

    Initial Commit

---
+++ oc2mango/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  oc2mango
+//
+//  Created by Jiang on 2019/4/10.
+//  Copyright © 2019年 SilverFruity. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
