7e591a08abaf7a60a5759ff2f4c1a30ebe99771a
Jiang <15328044115>
2020-11-02 14:51:29 +0800

    fix: [增加json文件补丁工具](https://github.com/SilverFruity/oc2mango/pull/2)

--- PatchGenerator/main.swift
+++ PatchGenerator/main.swift
@@ -32,56 +32,106 @@ func recursiveSanFiles(path: String) -> [String]{
     }
     return files
 }
+enum PatchType: String{
+    case json = "json"
+    case binary = "binary"
+}
+enum Options: String{
+    case files = "-files"
+    case refs = "-refs"
+    case output = "-output"
+    case type = "-type"
+    case encryptMap = "-encryptMap"
+    case h = "-h"
+    case help = "-help"
+}
 class CheckArgs{
-    var inputFiles = [String]()
-    var refrences = [String]()
+    var inputFilePaths = [String]()
+    var refrencePaths = [String]()
     var output = ""
-    
-    var isInputFiels = false
-    var isReferences = false
-    var isOutput = false
+    var type: PatchType = .binary
+    var encryptMap = ""
+    var isHelp = false
     init(args:[String]) {
-        for arg in args {
-             switch arg {
-             case "-files":
-                 isInputFiels = true
-                 isReferences = false
-                 isOutput = false
-                 continue
-             case "-refs":
-                 isInputFiels = false
-                 isReferences = true
-                 isOutput = false
-                 continue
-             case "-output":
-                 isInputFiels = false
-                 isReferences = false
-                 isOutput = true
-                 continue
-             default:
-                 break
-             }
-             if isInputFiels {
-                 inputFiles.append(contentsOf: recursiveSanFiles(path: arg))
-             }else if(isReferences){
-                 refrences.append(contentsOf: recursiveSanFiles(path: arg))
-             }else if (isOutput){
-                 output = arg
-             }
-         }
+        var curOption: Options? = .files
+        for arg in args{
+            if arg.hasPrefix("-") {
+                curOption = Options.init(rawValue: arg)
+                if curOption == .help || curOption == .h{
+                    self.isHelp = true
+                    self.showHelp()
+                }
+            }
+            switch curOption {
+            case .files:
+                self.inputFilePaths.append(arg)
+                break
+            case .refs:
+                self.refrencePaths.append(arg)
+                break
+            case .output:
+                self.output = arg
+            case .type:
+                if let type = PatchType.init(rawValue: arg){
+                    self.type = type
+                }
+                break
+            case .encryptMap:
+                self.encryptMap = arg
+                break
+            default:
+                break
+            }
+        }
+    }
+    func showHelp(){
+        print(
+        """
+
+        required:
+        -files: Objective-C source files.
+        -refs: same as header files, include C function declare, struct, enum, inline function etc..
+        -output: output patch file path.
+
+        optional:
+        -type: json or binary. default is binary.
+        -encryptMap: only for json patch. if use json patch, it must be exsited.
+
+        For example:
+
+        1. Binary Patch
+        ./PatchGenerator -files /user/mac/filesDir -refs /user/mac/headersDir -output /usr/mac/binarypatch
+
+        2. Json Patch
+        ./PatchGenerator -type json -encryptMap /user/mac/ClassEncryptMap.json -files /user/mac/filesDir -refs /user/mac/headersDir -output /usr/mac/jsonpatch
+
+        """)
     }
 }
 func main(){
     let result = CheckArgs.init(args: Array.init(CommandLine.arguments[1...]))
+    if result.isHelp {
+        return
+    }
+    var inputSourceFiles = [String]()
+    var inputRefrenceFiles = [String]()
+    for path in result.inputFilePaths{
+        inputSourceFiles.append(contentsOf: recursiveSanFiles(path: path))
+    }
+    for path in result.refrencePaths{
+        inputRefrenceFiles.append(contentsOf: recursiveSanFiles(path: path))
+    }
+    
     var input = ""
-    print("InputFiles:\n\(result.inputFiles)")
-    for path in result.inputFiles{
+    print("InputFiles:\n\(inputSourceFiles)")
+    for path in inputSourceFiles{
         let data = NSData.init(contentsOfFile: path)! as Data
         input += String.init(data: data, encoding: .utf8)!
     }
+    
     var reference = ""
-    print("References:\n\(result.refrences)")
-    for path in result.refrences{
+    print("References:\n\(inputRefrenceFiles)")
+    for path in inputRefrenceFiles{
         let data = NSData.init(contentsOfFile: path)! as Data
         reference += String.init(data: data, encoding: .utf8)!
     }
@@ -91,9 +141,19 @@ func main(){
     // for refs:
     let refNodes = parser.parseSource(reference).nodes as! [Any]
     let inputNodes = parser.parseSource(input).nodes as! [Any]
-    
     let patchFile = ORPatchFile.init(nodes: refNodes + inputNodes)
-    patchFile.dump(asBinaryPatch: result.output)
+    switch result.type {
+    case .binary:
+        patchFile.dump(asBinaryPatch: result.output)
+        break
+    case .json:
+        if result.encryptMap.count == 0{
+            print("必须传入encryptMap文件!")
+        }
+        patchFile.dump(asJsonPatch: result.output, encrptMapPath: result.encryptMap)
+        break
+    }
+    
 }
 main()
 
